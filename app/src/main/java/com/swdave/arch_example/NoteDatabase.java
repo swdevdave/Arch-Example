7af4bc0fced72e2130695e6ba1c5f0669e45016d
DaveMain <swdev.dave@gmail.com>
2018-10-02 15:45:42 -0700

    Added local Repo & Callback

--- app/src/main/java/com/swdave/arch_example/NoteDatabase.java
+++ app/src/main/java/com/swdave/arch_example/NoteDatabase.java
@@ -1,9 +1,12 @@
 package com.swdave.arch_example;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,9 +21,39 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
 
         }
         return instance;
+    }
+
+
+    // Callback to create initial data into the DB on Create
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            new PopulateDbAsyncTask(instance).execute();
+            super.onCreate(db);
+        }
+    };
+
+    // Async Task to call insert
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db){
+
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
     }
 }
